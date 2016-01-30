e71485ca7b9f9bed9d71e1bc548c97aad59cb973
connorbell <connor.n.bell@gmail.com>
2016-01-30 11:21:14 -0400

    Added lil scene

---
+++ GlobalGameJam2016/Assets/Scripts/CronMaze/FloorReset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorReset : MonoBehaviour {
+
+	public GameObject Player;
+	public PlatformManager Platformer;
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject == Player) {
+			Debug.Log("Reset!");
+			Platformer.Reset();
+		}
+	}
+
+}
---
+++ GlobalGameJam2016/Assets/Scripts/CronMaze/Platform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	public GameObject Player;
+	public PlatformManager Platformer;
+
+	public float Timeout = 5f;
+	
+	private float CurrentTime = 0.0f;
+	private bool timeoutStarted;
+
+	void Update () {
+		if (timeoutStarted) {
+			CurrentTime += Time.deltaTime;
+		}
+		if (CurrentTime >= Timeout) {
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject == Player) {
+			timeoutStarted = true;
+		}
+	}
+}
